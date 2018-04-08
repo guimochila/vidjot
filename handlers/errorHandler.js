f95083d5e7ce2dc56cad57930956fa320037fcc7
Guilherme <gui.mochila@gmail.com>
2018-04-08 11:47:43 +0100

    Refactored handlers

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -6,11 +6,14 @@
   catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
 */
 
-exports.catchErrors = (fn) => {
-	return function(req, res, next) {
-		return fn(req, res, next).catch(next);
-	};
-};
+// exports.catchErrors = fn => {
+// 	return function(req, res, next) {
+// 		return fn(req, res, next).catch(next);
+// 	};
+// };
+
+// For Eslint/Airbnb/Prettier the code bellow is the refactoring of the code commented above.
+exports.catchErrors = fn => (req, res, next) => fn(req, res, next).catch(next);
 
 /*
   Not Found Error Handler
@@ -18,16 +21,16 @@ exports.catchErrors = (fn) => {
   If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
 */
 exports.notFound = (req, res, next) => {
-	const err = new Error('Not found.');
-	err.status = 404;
-	next(err);
-}
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+};
 
 /*
 	Rendering errors
 */
 
-exports.errorRender = (err, req, res, next) => {
-	res.status(err.status || 500);
-	res.send(`${err.message}`)
-}
+exports.errorRender = (err, req, res) => {
+  res.status(err.status || 500);
+  res.send(`${err.message}`);
+};
